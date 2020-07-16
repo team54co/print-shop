9545ca8511ef424aad959420052db6bec36ca3c4
Tripple-A <biodun9@gmail.com>
2020-07-16 10:32:01 +0100

    added validations to model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,9 @@
+
 class Client < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :sec_no, presence: true
+  validates :country, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
 end
