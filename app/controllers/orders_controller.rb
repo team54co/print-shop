3a7a95eb07ee6fca76a051d95a9c3aa07031d1af
Tripple-A <biodun9@gmail.com>
2020-07-17 12:09:23 +0100

    added create new order view and show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+    def new
+        id = params[:client_id]
+        @order = Order.create(client_id: id)
+        redirect_to action: "show", id: @order.id
+    end
+
+
+    def show
+       @client = Client.find(params[:client_id])
+    end
+
+    def index
+    end
+
+    def destroy
+        @order = Order.find(params[:id])
+        @order.destroy
+        #redirect_to root_path
+    end
 end
