898c4cd03a0f8371d50bbf1f37b1aab96d56e1ed
Tripple-A <biodun9@gmail.com>
2020-07-17 21:45:07 +0100

    added jquery for forms

--- app/controllers/ordered_products_controller.rb
+++ app/controllers/ordered_products_controller.rb
@@ -7,27 +7,32 @@ class OrderedProductsController < ApplicationController
     end
 
     def create
-    #     order = Order.find(params[:order_id])
-    #     product_params = params[:ordered_product]
-    #     puts product_params
-    #     product = Product.new(
-    #         name: product_params['name'],
-    #         price:  product_params['price'],
-    #         description: product_params['description']
-    #     )
-    #     if product.save
-    #         product_params['specific']['product_id'] = product.id
-    #         product_params['specific']['order_id'] = params[:order_id]
-    #         OrderedProduct.create(
-    #             product_id: product.id,
-    #             order_id: params[:order_id],
-    #             quantity: product_params['specific']['quantity'],
-    #             alterations: product_params['specific']['quantity'],
-    #             status: 'Processing'
-    #         )
-    #         redirect_to order
-    #     else
-    #         render new
-    #     end
-     end
+        order = Order.find(params[:order_id])
+        product_params = params[:ordered_product]
+        puts product_params
+        if product_params['product_id'] == '0'
+        product = Product.new(
+            name: product_params['name'],
+            price:  product_params['price'],
+            description: product_params['description']
+        )
+        end
+        if product&.save or product_params['product_id'] != 0
+            if product
+                id = product.id
+            else
+                id = product_params['product_id']
+            end
+            OrderedProduct.create(
+                product_id: id,
+                order_id: params[:order_id],
+                quantity: product_params['specific']['quantity'],
+                alterations: product_params['specific']['alterations'],
+                status: 'Processing'
+            )
+            redirect_to order
+        else
+            render new
+        end
+    end
 end
