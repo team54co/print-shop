f6c5dbf6cc71b7d5b7b82641bfdf27af6ab470d2
alexawesomecode <alexawesomecode@gmail.com>
2020-07-21 21:49:13 +0000

    new client styling

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,9 +1,13 @@
 class ClientsController < ApplicationController
-    def show
-        @client = Client.find(params[:id])
-    end
+  def show
+    @client = Client.find(params[:id])
+  end
 
-    def index
-        @clients = Client.all
-    end
+  def index
+    @clients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
 end
