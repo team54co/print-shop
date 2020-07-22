f8f52a662f15e4707f4b99022f01c017aed17fc3
Tripple-A <biodun9@gmail.com>
2020-07-22 04:53:05 +0100

    created edit form and update product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,19 @@ class ProductsController < ApplicationController
         @product = Product.find(params[:id])
 
     end
+
+    def edit
+        @product = Product.find(params[:id])
+      end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+          redirect_to(@product)
+        else
+          render "edit"
+        end
+      end
     
     def create
       product = Product.new(product_params)
