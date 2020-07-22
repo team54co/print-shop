c951ec3f49ac180d54b6e88d772ac241046af543
Tripple-A <biodun9@gmail.com>
2020-07-22 04:24:27 +0100

    added view and controller method for new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,23 @@
 class ProductsController < ApplicationController
+    def new
+        @product = Product.new
+    end
+
+    def show
+
+    end
+    
     def create
-        
+      product = Product.new(product_params)
+      if product.save
+        redirect_to product
+      else
+        render new
+      end  
+    end
+
+    private
+    def product_params
+        params.require(:product).permit(:name, :price, :description)
     end
 end
