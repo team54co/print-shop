241efa303e08474ad17cdd2074e912cfff919702
Tripple-A <biodun9@gmail.com>
2020-07-24 11:15:01 +0100

    added validations for numericality of price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: true
   has_many :ordered_products
   has_many :orders, through: :ordered_products, source: :order
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   context 'validation test' do
     it 'ensures name presence' do
-      user = Product.new(name: '', price: '123', description: 'A test product').save
-      expect(user).to eq(false)
+      product = Product.new(name: '', price: '123', description: 'A test product').save
+      expect(product).to eq(false)
+    end
+
+    it 'ensures price is numerical' do
+      product = Product.new(name: 'Test Product', price: 'a girl', description: 'A test product').save
+      expect(product).to eq(false)
     end
   end
 end
