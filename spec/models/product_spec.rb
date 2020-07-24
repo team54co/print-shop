475df90ffc9ea8bdc32ec202a92c56cc1b3a1cdc
Tripple-A <biodun9@gmail.com>
2020-07-24 11:02:08 +0100

    added validation test for presence of name in product creation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it 'ensures name presence' do
+      user = Product.new(name: '', price: '123', description: 'A test product').save
+      expect(user).to eq(false)
+    end
+  end
 end
