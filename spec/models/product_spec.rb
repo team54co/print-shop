0ff3430e12c8c05814b3a49d128f0467b532d1eb
Tripple-A <biodun9@gmail.com>
2020-07-24 11:43:39 +0100

    added extra validation error for numericality

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,24 +3,25 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   context 'validation test' do
     it 'ensures name presence' do
-      product = Product.new(name: '', price: '123', description: 'A test product').save
-      expect(product).to eq(false)
+      product = Product.new(name: '', price: '123', description: 'A test product')
+      expect(product).to_not be_valid
     end
 
     it 'ensures price is numerical' do
-      product = Product.new(name: 'Test Product', price: 'a girl', description: 'A test product').save
-      expect(product).to eq(false)
+      product = Product.new(name: 'Test Product', price: 'a girl', description: 'A test product')
+      expect(product).to_not be_valid
+      expect(product.errors[:price]).to include('is not a number')
     end
 
     it 'ensures price is present' do
-      product = Product.new(name: 'Test Product', price: '', description: 'A test product').save
-      expect(product).to eq(false)
+      product = Product.new(name: 'Test Product', price: '', description: 'A test product')
+      expect(product).to_not be_valid
     end
 
 
     it 'ensures description is present' do
-      product = Product.new(name: 'Test Product', price: 700, description: '').save
-      expect(product).to eq(false)
+      product = Product.new(name: 'Test Product', price: 700, description: '')
+      expect(product).to_not be_valid
     end
   end
 end
