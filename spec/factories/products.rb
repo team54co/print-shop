767126527555750cac30464170440068bb94fad7
Tripple-A <biodun9@gmail.com>
2020-07-24 10:54:50 +0100

    added factory faker data

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :product do
-    
+    name { Faker::Lorem.sentence(word_count: 3) }
+    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    price { Faker::Number.number(digits: 3) }
   end
 end
